75ca46cb5307c99b001a01813ca0872dc10b4def
DIWAN ANUJ JITENDRA <anujdiwan2008@gmail.com>
2019-04-16 00:40:18 +0530

    Made knobs

--- keyboardEvent.rkt
+++ keyboardEvent.rkt
@@ -1,13 +1,38 @@
 #lang racket/gui
-(provide (all-defined-out))
-(define (key-handle key-event) 
-     (define key-value (send key-event get-key-code))
-     (define listed (string->list (~a key-value)))
-     (cond
-       [(equal? key-value 'release)
-         'done]
-        [(and (= 1 (length listed))
-         (char-alphabetic? (car listed)))
-         (define character (char-upcase (car listed)))
-         'send-char]
-        ))
+(require 2htdp/image)
+
+(define knob%
+  (class canvas%
+    (init-field i)
+    (super-new)
+    (inherit get-dc get-width get-height)
+    (define current 0)
+
+    (define knob (make-object bitmap% "26.png"))
+    (define num 0)
+
+    (define/override (on-paint)
+      (define dc (get-dc))
+      (define w (get-width))
+      (define h (get-height))
+        (send dc draw-bitmap
+               knob 0 0)
+      )
+      ))
+
+
+(define frame (new frame%
+                   [label "Example"]
+                   [width 750]
+                   [height 225]))
+(new knob%
+     [parent frame]
+     [i 0])
+(new knob%
+     [parent frame]
+     [i 1])
+(new knob%
+     [parent frame]
+     [i 2])
+
+(send frame show #t)
