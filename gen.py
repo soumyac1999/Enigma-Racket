d7ed40d1006d798fed85430c8b1a700f07957d4d
DIWAN ANUJ JITENDRA <anujdiwan2008@gmail.com>
2019-04-20 19:51:59 +0530

    Made knobs

---
+++ gen.py
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		img{
+			transform: rotate(13.84615deg);
+		}
+	</style>
+</head>
+<body>
+<div id="a">
+	<img src="26.png">
+</div>
+</body>
+</html>
--- keyboardEvent.rkt
+++ keyboardEvent.rkt
@@ -1,38 +1,56 @@
 #lang racket/gui
 (require 2htdp/image)
+(define i 10)
+(define j 12)
+(define k 1)
 
+(define (get-value num)
+  (cond [(= num 0) i]
+        [(= num 1) j]
+        [(= num 2) k]))
 (define knob%
   (class canvas%
     (init-field i)
     (super-new)
     (inherit get-dc get-width get-height)
     (define current 0)
-
-    (define knob (make-object bitmap% "26.png"))
-    (define num 0)
-
     (define/override (on-paint)
       (define dc (get-dc))
+      (define knob (make-object bitmap% (string-append
+                                       "circle-"
+                                       (~a (get-value i)) ".png-t")))
+      (define knob-w (image-width knob))
+      (define knob-h (image-height knob))
       (define w (get-width))
+      (display w)
+      (newline)
       (define h (get-height))
+      (display h)
+      (newline)
         (send dc draw-bitmap
-               knob 0 0)
+               knob (/ (- w knob-w) 2) (/ (- h knob-h) 2))
       )
       ))
-
-
 (define frame (new frame%
                    [label "Example"]
                    [width 750]
                    [height 225]))
+(define panel (new horizontal-panel%
+                   [parent frame]))
 (new knob%
-     [parent frame]
-     [i 0])
+     [parent panel]
+     [i 0]
+     [style (list 'border)]
+     )
 (new knob%
-     [parent frame]
-     [i 1])
+     [parent panel]
+     [i 1]
+     [style (list 'border)]
+     )
 (new knob%
-     [parent frame]
-     [i 2])
+     [parent panel]
+     [i 2]
+     [style (list 'border)]
+     )
 
 (send frame show #t)
