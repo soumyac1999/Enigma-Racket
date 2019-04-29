acfec7d8967b25525854d50cdc0ed35cfe28b16a
Soumya Chatterjee <soumyac1999@gmail.com>
2019-04-29 17:00:50 +0530

    Added button images

--- control.rkt
+++ control.rkt
@@ -11,15 +11,15 @@
     (super-new)
     (inherit get-dc get-width get-height)
 
-    (define en (make-object bitmap% "white.jpeg"))
-    (define de (make-object bitmap% "white.jpeg"))
-    (define cr (make-object bitmap% "white.jpeg"))
+    (define en (make-object bitmap% "en.png"))
+    (define de (make-object bitmap% "de.png"))
+    (define cr (make-object bitmap% "cr.png"))
 
     (define/override (on-paint)
       (define dc (get-dc))
       (send dc draw-bitmap en 0 0)
       (send dc draw-bitmap de 0 76)
-      (send dc draw-bitmap de 0 152))
+      (send dc draw-bitmap cr 0 152))
 
     (define/override (on-event mouse-event)
       (define dc (get-dc))
